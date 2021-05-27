fc07a3346504fa5e54a83af818f68c11fbfbdfac
abrabant <abrabant@student.42.fr>
2021-05-27 18:28:08 +0200

    Print death in bold red to improve visibility

--- lib/src/get_state_string.c
+++ lib/src/get_state_string.c
@@ -5,7 +5,7 @@ const char	*get_state_string(t_philo_state state)
 	if (state == PHILO_STATE_EATING)
 		return (" is eating\n");
 	if (state == PHILO_STATE_DEAD)
-		return (" died\n");
+		return (" \033[1;31mdied\033[0m\n");
 	if (state == PHILO_STATE_SLEEPING)
 		return (" is sleeping\n");
 	if (state == PHILO_STATE_THINKING)
